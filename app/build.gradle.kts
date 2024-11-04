9d519766c5baa1f2e2ae53e82592c489b9b0eb3b
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-11-04 11:13:53 +0700

    finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.dicoding.latihantensorflowliteprediction"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dicoding.latihantensorflowliteprediction"
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.play.services.tflite.java)
+    implementation(libs.play.services.tflite.gpu)
 }
